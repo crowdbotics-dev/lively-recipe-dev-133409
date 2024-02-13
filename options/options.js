c193a7373b3e99b1e1356606669acfdbfb8d28a2
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-recipe-dev-133409",
+  url: "https://lively-recipe-dev-133409.botics.co",
+  api: "https://lively-recipe-dev-133409.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
